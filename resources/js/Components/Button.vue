df1f40a390113863e09220447bed10e7ae5afd68
Sebastiaan <sebas.nolte@hotmail.com>
2021-10-02 15:56:41 +0200

    Update - New guest layout = New font

--- resources/js/Components/Button.vue
+++ resources/js/Components/Button.vue
@@ -1,16 +1,36 @@
 <template>
-    <button :type="type" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150">
-        <slot />
-    </button>
+  <button
+    :type="type"
+    class="
+      flex
+      items-center
+      text-center
+      justify-center
+      px-4
+      py-2
+      border border-transparent
+      rounded-md
+      shadow-sm
+      text-sm
+      font-medium
+      text-white
+      bg-indigo-600
+      hover:bg-indigo-700
+      focus:outline-none
+      focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
+    "
+  >
+    <slot />
+  </button>
 </template>
 
 <script>
-    export default {
-        props: {
-            type: {
-                type: String,
-                default: 'submit',
-            },
-        }
-    }
+export default {
+  props: {
+    type: {
+      type: String,
+      default: "submit",
+    },
+  },
+};
 </script>
--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,26 +1,16 @@
 <template>
   <Head title="Login" />
-  <div class="flex justify-center">
+  <div class="">
     <div
-      class="
-        px-4
-        mb-4
-        leading-4
-        bg-white
-        p-4
-        shadow
-        rounded-md
-        sm:overflow-hidden
-        w-full
-        sm:w-2/3
-      "
+      class="mb-4 leading-4 bg-white p-10 shadow rounded-md sm:overflow-hidden"
     >
       <breeze-validation-errors class="mb-4" />
-
       <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
         {{ status }}
       </div>
-
+      <h2 class="mb-6 text-lg leading-9 text-gray-900">
+        Sign in to your account
+      </h2>
       <form @submit.prevent="submit">
         <div>
           <breeze-label for="email" value="Email" />
@@ -34,9 +24,17 @@
             autocomplete="username"
           />
         </div>
-
         <div class="mt-4">
-          <breeze-label for="password" value="Password" />
+          <div class="flex justify-between">
+            <breeze-label for="password" value="Password" />
+            <inertia-link
+              v-if="canResetPassword"
+              :href="route('password.request')"
+              class="underline text-sm text-gray-600 hover:text-gray-900"
+            >
+              Forgot your password?
+            </inertia-link>
+          </div>
           <breeze-input
             id="password"
             type="password"
@@ -55,21 +53,13 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-          <inertia-link
-            v-if="canResetPassword"
-            :href="route('password.request')"
-            class="underline text-sm text-gray-600 hover:text-gray-900"
-          >
-            Forgot your password?
-          </inertia-link>
-
-          <breeze-button
-            class="ml-4"
+          <Button
+            class="w-full"
             :class="{ 'opacity-25': form.processing }"
             :disabled="form.processing"
           >
             Log in
-          </breeze-button>
+          </Button>
         </div>
       </form>
     </div>
@@ -83,6 +73,7 @@ import BreezeInput from "@/Components/Input";
 import BreezeCheckbox from "@/Components/Checkbox";
 import BreezeLabel from "@/Components/Label";
 import BreezeValidationErrors from "@/Components/ValidationErrors";
+import Button from "@/Components/Button.vue";
 
 export default {
   layout: Layout,
@@ -93,6 +84,7 @@ export default {
     BreezeCheckbox,
     BreezeLabel,
     BreezeValidationErrors,
+    Button,
   },
 
   props: {
