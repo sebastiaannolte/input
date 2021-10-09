d4d92a16f956bf25752ab33bb9936c2fa3408778
Sebastiaan <sebas.nolte@hotmail.com>
2021-10-09 09:31:08 +0200

    Disable middleware for now

--- app/Http/Middleware/DomainCheckMiddleware.php
+++ app/Http/Middleware/DomainCheckMiddleware.php
@@ -20,19 +20,19 @@ class DomainCheckMiddleware
     {
         $allowedHosts = explode(',', env('ALLOWED_DOMAINS'));
         $requestHost = parse_url(request()->headers->get('origin'),  PHP_URL_HOST);
-        dd($allowedHosts, $requestHost);
-        if(!app()->runningUnitTests() && App::environment() != 'local') {
-            if(!in_array($requestHost, $allowedHosts, false)) {
-                $requestInfo = [
-                    'host' => $requestHost,
-                    'ip' => $request->getClientIp(),
-                    'url' => $request->getRequestUri(),
-                    'agent' => $request->header('User-Agent'),
-                ];
-                abort(403, 'Unauthorized action.');
-                throw new Exception('This host is not allowed');
-            }
-        }
+        // dd($allowedHosts, $requestHost);
+        // if(!app()->runningUnitTests() && App::environment() != 'local') {
+        //     if(!in_array($requestHost, $allowedHosts, false)) {
+        //         $requestInfo = [
+        //             'host' => $requestHost,
+        //             'ip' => $request->getClientIp(),
+        //             'url' => $request->getRequestUri(),
+        //             'agent' => $request->header('User-Agent'),
+        //         ];
+        //         abort(403, 'Unauthorized action.');
+        //         throw new Exception('This host is not allowed');
+        //     }
+        // }
 
         return $next($request);
     }
