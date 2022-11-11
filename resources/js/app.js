82d38931c950df25e512a809fee9528631809f27
Sebastiaan <sebas.nolte@hotmail.com>
2022-11-11 21:31:32 +0100

    Laravel 9

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,34 +1,34 @@
-require('./bootstrap');
+require('./bootstrap')
 
-import { createApp, h } from 'vue';
-import { createInertiaApp, Head, Link } from '@inertiajs/inertia-vue3';
-import { InertiaProgress } from '@inertiajs/progress';
+import { createApp, h } from 'vue'
+import { createInertiaApp, Head, Link } from '@inertiajs/inertia-vue3'
+import { InertiaProgress } from '@inertiajs/progress'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import mitt from 'mitt';
-import Toaster from "@meforma/vue-toaster";
-const emitter = mitt();
-import { Chart, LineController, LineElement, PointElement, LinearScale, Title, CategoryScale } from 'chart.js';
-Chart.register(LineController, LineElement, PointElement, LinearScale, Title, CategoryScale);
+import mitt from 'mitt'
+import Toaster from '@meforma/vue-toaster'
+const emitter = mitt()
+import { Chart, LineController, LineElement, PointElement, LinearScale, Title, CategoryScale } from 'chart.js'
+Chart.register(LineController, LineElement, PointElement, LinearScale, Title, CategoryScale)
 
-const regeneratorRuntime = require("regenerator-runtime");
-const el = document.getElementById('app');
+const regeneratorRuntime = require('regenerator-runtime')
+const el = document.getElementById('app')
 
 createInertiaApp({
-    title: title => `${title} | ${process.env.MIX_APP_NAME}`,
-    resolve: (name) => require(`./Pages/${name}`),
-    setup({ el, app, props, plugin }) {
-        const apps = createApp({ render: () => h(app, props) })
-            .mixin({ methods: { route } })
-            .use(plugin)
-            .component('Head', Head)
-            .component('InertiaLink', Link)
-            .use(VueAxios, axios)
-            .use(Toaster);
+  title: title => `${title} | ${process.env.MIX_APP_NAME}`,
+  resolve: (name) => require(`./Pages/${name}`),
+  setup({ el, app, props, plugin }) {
+    const apps = createApp({ render: () => h(app, props) })
+      .mixin({ methods: { route } })
+      .use(plugin)
+      .component('Head', Head)
+      .component('InertiaLink', Link)
+      .use(VueAxios, axios)
+      .use(Toaster)
 
-        apps.config.globalProperties.emitter = emitter;
-        apps.mount(el);
-    },
-});
+    apps.config.globalProperties.emitter = emitter
+    apps.mount(el)
+  },
+})
 
-InertiaProgress.init({ color: '#4B5563' });
+InertiaProgress.init({ color: '#4B5563' })
