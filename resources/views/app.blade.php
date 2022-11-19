256a2d53fa6495f213c769313bfcf62cf00817aa
Sebastiaan <sebas.nolte@hotmail.com>
2022-11-19 17:18:21 +0100

    Test with new meta

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,8 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- Fonts -->
