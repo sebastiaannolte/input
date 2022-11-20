b71ffc48678616e5ab74d196a0ebc5ad7c0cc91a
Sebastiaan <sebas.nolte@hotmail.com>
2022-11-20 23:01:04 +0100

    Meta test

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+    <meta name="viewport" content="user-scalable=no, initial-scale=1.0, viewport-fit=cover" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 
