4cc7a8b43012a8071c09bdd50186b1ac053b16f0
Sebastiaan <sebas.nolte@hotmail.com>
2022-11-20 22:48:49 +0100

    Meta test

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="mobile-wep-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 
     <!-- Fonts -->
