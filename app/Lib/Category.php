f906c94746d4bf867d3d6079320f9543549a994b
Sebastiaan <sebas.nolte@hotmail.com>
2025-08-14 18:44:30 +0200

    Better mapping

--- app/Lib/Category.php
+++ app/Lib/Category.php
@@ -17,11 +17,16 @@ class Category
             'Scoort: ' => 83,
             'Scoort ten minste 2 doelpunten: ' => 323,
             'Scoort ten minste 3 doelpunten: ' => 324,
-            'Draw No Bet:  ' => 544,
+            'Draw No Bet: ' => 544,
             'Scoort of Geeft Een Assist ' => 364,
             'Schoten van Speler op Doel, ' => 371,
             'Dubbele Kans: ' => 12,
             'Totaal Aantal Doelpunten: ' => 5,
+            'Beide Teams Scoren: ' => 8,
+            'Beide Teams Scoren - 1e Helft: ' => 34,
+            'Beide Teams Scoren - 2e Helft: ' => 35,
+            'Team Bereikt Volgende Ronde: ' => 61,
+            'Rust/Eindresultaat: ' => 7,
         ];
 
         foreach ($mapping as $key => $value) {
