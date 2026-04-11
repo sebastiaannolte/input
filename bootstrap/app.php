823d4b347893258616e95aad06c77f86aa568606
Sebastiaan <sebas.nolte@hotmail.com>
2026-04-11 18:22:11 +0200

    Temp disable sentry

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -57,15 +57,17 @@ $app->singleton(
 |
 */
 
-return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        Integration::handles($exceptions);
-    })->create();
+// return Application::configure(basePath: dirname(__DIR__))
+//     ->withRouting(
+//         web: __DIR__.'/../routes/web.php',
+//         commands: __DIR__.'/../routes/console.php',
+//         health: '/up',
+//     )
+//     ->withMiddleware(function (Middleware $middleware) {
+//         //
+//     })
+//     ->withExceptions(function (Exceptions $exceptions) {
+//         Integration::handles($exceptions);
+//     })->create();
+
+return $app;
