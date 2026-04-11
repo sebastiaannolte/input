a6b174715c6c8bbc26664c96f3198a820f5904fa
Sebastiaan <sebas.nolte@hotmail.com>
2026-04-11 16:48:24 +0200

    Added sentry

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,10 @@
 <?php
 
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+use Sentry\Laravel\Integration;
+
 /*
 |--------------------------------------------------------------------------
 | Create The Application
@@ -52,4 +57,15 @@ $app->singleton(
 |
 */
 
-return $app;
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        //
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        Integration::handles($exceptions);
+    })->create();
